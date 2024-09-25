fc842f5bb79a3eb75dcf20a59a6b1d8e6520b2bd
Theotime.Lebreton <theotime.lebreton@gmail.com>
2024-09-25 15:07:09 +0200

    Initial Project Version

---
+++ src/test/java/com/SafetyNet/Projet5/Projet5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SafetyNet.Projet5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
